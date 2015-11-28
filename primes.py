b4e1d1ab7002ec66650b98cbc5ef0d27c3c02fcc
tonatonantzin <tonatonantzin@gmail.com>
2015-11-28 00:23:51 -0600

    Update primes.py

--- primes.py
+++ primes.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+# Tonantzin Chavez 11/26/2015
+# Calcula si un numero es primo o no
+# Devuelve True o False
 
 class PrimeClass(object):
-    """
-    Your class documentation here
-    """
+   
+
     def is_prime(num):
-        for i in range(2,num):
-            if (num%i)==0:
+        for i in range(2, num):
+            if (num % i) == 0:
                 return False
         return True
-     
+
     while True:
         try:
             num = int(raw_input("Escribe numero => "))
-            if num==0:
+            if num == 0:
                 break
             if is_prime(num):
-                print  is_prime(num)
+                print is_prime(num)
             else:
-               print  is_prime(num)
+               print is_prime(num)
         except:
-            print "Valor erroneo" 
+            print "Valor erroneo"
