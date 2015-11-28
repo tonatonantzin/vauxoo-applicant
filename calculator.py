fd6ffb232d67cbea401b5b3b5ec3185e795d6da5
tonatonantzin <tonatonantzin@gmail.com>
2015-11-28 12:36:29 -0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # Tonantzin Chavez 11/26/2015
-# Calcula la suma de 2 numeros enteros
+# Calcula la suma de una lista de numeros enteros
 
 
-class CalculatorClass(object):
+class CalculatorClass:
+   lista = []
+   suma = 0
+   def llenarlista(self):
+       x= int (raw_input("Ingrese longitud de la lista=>"))
+       for i in range(x):
+           self.lista.append(int(raw_input("Numero=>")))
 
-    def sum(x, y):
-        res = x + y
-        return res
-    while True:
-        try:
-            x = int(input("Ingrese Numero: "))
-            y = int(input("Ingrese Otro Numero: "))
-            print sum(x, y)
-        except:
-            print "Valor erroneo"
+
+   def sum(self):
+       for i in self.lista:
+           self.suma+= i
+       print "Total=>",self.suma
+
+
+test = CalculatorClass()
+test.llenarlista()
+test.sum()
