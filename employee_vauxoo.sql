7c8ba7c655d3448dd5ded78ac7aa73a404ce3de6
tonatonantzin <tonatonantzin@gmail.com>
2015-11-28 00:04:17 -0600

    Update employee_vauxoo.sql

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -4,13 +4,68 @@
 --       Consider add ';' at end sentence.
 
 
---sql1
+-- sql1
 CREATE TABLE IF NOT EXISTS employee (
-  id int PRIMARY KEY NOT NULL,
+  id int  NOT NULL PRIMARY KEY,
   first_name char(15) NOT NULL,
   last_name char(15) NOT NULL,
   id_deparment int references employee_deparment(id)
-) 
+);
+
+
+CREATE TABLE IF NOT EXISTS employee_deparment (
+  id int NOT NULL PRIMARY KEY ,
+  name varchar(30) NOT NULL,
+  description varchar(50) DEFAULT NULL
+) ;
+
+
+-- sql2
+
+CREATE TABLE IF NOT EXISTS employee_hobby (
+  id int  NOT NULL PRIMARY KEY,
+  name varchar(30) NOT NULL,
+  description varchar(60) DEFAULT NULL
+) ;
+
+
+CREATE TABLE IF NOT EXISTS employee_hobbyrel (
+  id_employee int NOT NULL REFERENCES employee(id),
+  id_hobby int NOT NULL REFERENCES employee_hobby(id)
+) ;
+
+-- Sql3
+CREATE TABLE IF NOT EXISTS employee_boss (
+  id int  NOT NULL PRIMARY KEY,
+  id_employee int NOT NULL REFERENCES employee (id)
+) ;
+
+
+CREATE TABLE IF NOT EXISTS employee_bossrel (
+  id_boss int NOT NULL REFERENCES employee_boss(id),
+  id_employee int NOT NULL REFERENCES employee(id)
+) ;
+
+
+-- Relaciones
+
+ALTER TABLE employee
+  ADD CONSTRAINT employee_ibfk_1 FOREIGN KEY (id_deparment) REFERENCES employee_deparment (id) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
+ALTER TABLE employee_hobbyrel
+  ADD CONSTRAINT employee_hobbyrel_ibfk_1 FOREIGN KEY (id_hobby) REFERENCES employee_hobby (id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT employee_hobbyrel_ibfk_2 FOREIGN KEY (id_employee) REFERENCES employee (id) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
+ALTER TABLE employee_boss
+  ADD CONSTRAINT ‘employee_boss_ibfk_1’ FOREIGN KEY (id_employee) REFERENCES employee (id) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE employee_bossrel
+  ADD CONSTRAINT employee_bossrel_ibfk_1 FOREIGN KEY (id_boss) REFERENCES employee_boss (id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT employee_bossrel_ibfk_2 FOREIGN KEY (id_employee) REFERENCES employee (id) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
 
 -- Valores
 INSERT INTO employee (id, first_name, last_name, id_deparment) VALUES
@@ -20,14 +75,7 @@ INSERT INTO employee (id, first_name, last_name, id_deparment) VALUES
 (4, 'VICENTE', 'SANCHEZ', 2);
 
 
-CREATE TABLE IF NOT EXISTS employee_deparment (
-  ‘id’ int PRIMARY KEY NOT NULL,
-  ‘name’ varchar(30) NOT NULL,
-  ‘description’ varchar(50) DEFAULT NULL
-) 
-
---Valores
-INSERT INTO employee_deparment (‘id’, ‘name’, ‘description’) VALUES
+INSERT INTO employee_deparment (id, name, description) VALUES
 (1, 'COMPRAS', 'Departamento encargado de hacer las compras'),
 (2, 'RECURSOS HUMANOS', 'Departamento par control de personal'),
 (3, 'CALIDAD OE', 'Departamento encargado de revisar los equipos OE'),
@@ -36,39 +84,13 @@ INSERT INTO employee_deparment (‘id’, ‘name’, ‘description’) VALUES
 (6, 'MARKETING', 'Departamento encargado de publicidad');
 
 
-
-
-ALTER TABLE ‘employee’
-  ADD CONSTRAINT ‘employee_ibfk_1’ FOREIGN KEY (‘id_deparment’) REFERENCES ‘employee_deparment’ (‘id’) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-
-
-
-
---sql2
-
-
-CREATE TABLE IF NOT EXISTS employee_hobby (
-  ‘id’ int PRIMARY KEY NOT NULL,
-  ‘name’ varchar(30) NOT NULL,
-  ‘description’ varchar(60) DEFAULT NULL
-) 
-
---Valores
-INSERT INTO employee_hobby (‘id’, ‘name’, ‘description’) VALUES
+INSERT INTO employee_hobby (id, name, description) VALUES
 (1, 'LECTURA', 'El acto de leer'),
 (2, 'CORRER', 'Hacer ejercicio'),
 (3, 'BAILAR', 'Diferentes generos');
 
 
-
-CREATE TABLE IF NOT EXISTS employee_hobbyrel (
-  ‘id_employee’ int NOT NULL REFERENCES employee(id),
-  ‘id_hobby’ int NOT NULL REFERENCES employee_hobby(id)
-) 
-
---Valores
-INSERT INTO employee_hobbyrel (‘id_employee’, ‘id_hobby’) VALUES
+INSERT INTO employee_hobbyrel (id_employee, id_hobby) VALUES
 (1, 1),
 (1, 2),
 (2, 1),
@@ -79,40 +101,13 @@ INSERT INTO employee_hobbyrel (‘id_employee’, ‘id_hobby’) VALUES
 (4, 1),
 (4, 3);
 
---Relaciones
-ALTER TABLE ‘employee’
-  ADD CONSTRAINT ‘employee_ibfk_1’ FOREIGN KEY (‘id_deparment’) REFERENCES ‘employee_deparment’ (‘id’) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-ALTER TABLE ‘employee_hobbyrel’
-  ADD CONSTRAINT ‘employee_hobbyrel_ibfk_1’ FOREIGN KEY (‘id_hobby’) REFERENCES ‘employee_hobby’ (‘id’) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT ‘employee_hobbyrel_ibfk_2’ FOREIGN KEY (‘id_employee’) REFERENCES ‘employee’ (‘id’) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-
-CREATE TABLE IF NOT EXISTS ‘employee_boss’ (
-  ‘id’ int PRIMARY KEY NOT NULL,
-  ‘id_employee’ int NOT NULL REFERENCES employee (id)
-) 
-
---Valores
-INSERT INTO employee_boss (‘id’, ‘id_employee’) VALUES
+INSERT INTO employee_boss (id, id_employee) VALUES
 (2, 2),
 (1, 3);
 
 
-CREATE TABLE IF NOT EXISTS ‘employee_bossrel’ (
-  ‘id_boss’ int NOT NULL REFERENCES employee_boss(id),
-  ‘id_employee’ int NOT NULL REFERENCES employee(id)
-) 
-
---Valores
-INSERT INTO ‘employee_bossrel’ (‘id_boss’, ‘id_employee’) VALUES
+INSERT INTO employee_bossrel (id_boss, id_employee) VALUES
 (1, 3),
 (1, 2);
 
---Relaciones
-ALTER TABLE ‘employee_boss’
-  ADD CONSTRAINT ‘employee_boss_ibfk_1’ FOREIGN KEY (‘id_employee’) REFERENCES ‘employee’ (‘id’) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-ALTER TABLE ‘employee_bossrel’
-  ADD CONSTRAINT ‘employee_bossrel_ibfk_1’ FOREIGN KEY (‘id_boss’) REFERENCES ‘employee_boss’ (‘id’) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT ‘employee_bossrel_ibfk_2’ FOREIGN KEY (‘id_employee’) REFERENCES ‘employee’ (‘id’) ON DELETE NO ACTION ON UPDATE NO ACTION;
