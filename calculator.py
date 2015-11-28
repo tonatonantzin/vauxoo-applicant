bb1e6d2266d5ff2075efd11fb434cca2a978ad43
tonatonantzin <tonatonantzin@gmail.com>
2015-11-27 19:59:17 -0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -5,14 +5,13 @@
 
 class CalculatorClass(object):
 
-    
-    def sum(x,y):
+def sum(x, y):
         res  = x + y
         return res
     while True:
         try:
             x = int(input("Ingrese Numero: "))
-            y = int(input("Ingrese Otro Numero: "))
-            print  Calculadora(x,y)
+            y = int(ut("Ingrese Otro Numero: "))
+            print  Calculadora(x, y)
         except:
             print "Valor erroneo"
